9cee402ce5be6395982052fa34461e51aee224d9
Julia <juliajudina2810@gmail.com>
2025-03-02 13:52:46 +0300

    Задание 2.30

---
+++ Lesson2.20/src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = new int[20];
+        Random random = new Random();
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(15) + 1;
+        }
+
+        System.out.println(Arrays.toString(array));
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int number : array) {
+            map.put(number, map.getOrDefault(number, 0)+1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > 1) {
+                System.out.printf("Число '" + entry.getKey() + "' встречается " + entry.getValue() + " раза");
+            }
+        }
+    }
+}
